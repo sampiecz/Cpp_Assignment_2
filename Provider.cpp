0f3ffe199eb222056e3c06a55b3241da1d106fbf
Samuel Piecz <z1732715@turing.cs.niu.edu>
2017-09-18 23:00:51 -0500

    Initial commit.  Working on project setup.

---
+++ Provider.cpp
@@ -0,0 +1,11 @@
+/***********************************************************
+ CSCI 241 - Assignment 2 - Fall 2017
+ 
+ Progammer: Sam Piecz
+ Z-ID: Z1732715
+ Section: 1
+ TA: Sumaiya Abdul
+ Date Due: September 26, 2017
+ 
+ Purpose: Not quite sure yet.  Some continuation of providers.
+ ************************************************************/
---
+++ Provider.h
@@ -0,0 +1,45 @@
+#ifndef PROVIDER_H
+#define PROVIDER_H
+
+//*********************************************************************
+// FILE:	Provider.h
+// AUTHOR:	Samuel Piecz
+// LOGON ID:	Z1732715
+// DUE DATE:	9/26/17
+//
+// PURPOSE:	Declaration for the Provider class, which represents
+//		information about a health care provider.
+//********************************************************************
+
+class Provider
+    {
+        private:
+            //Data members
+            char number[7] = {};
+            char specialty[41] = {};
+            char name[41] = {};
+            char address_1[41] = {};
+            char address_2[31] = {};
+            char city[21] = {};
+            char state[3] = {};
+            char zip_code[6] = {};
+            char phone_number[15] = {};
+
+        public:
+            //Method prototypes
+
+            //Default constructor
+            Provider();
+
+            //Print provider data
+            void print() const;
+
+            //Accessor method returns provider number data member
+            int getProviderNumber() const;
+
+            //Accessor method that returns specialty data member
+            int getName() const;
+
+    };
+
+#endif
