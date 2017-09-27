71529be10969ec262d8da6d7a0fcb27c608dc857
Samuel Piecz <z1732715@turing.cs.niu.edu>
2017-09-27 18:09:40 -0500

    Added comments to provider.h

--- Provider.h
+++ Provider.h
@@ -5,14 +5,17 @@
 // FILE:	Provider.h
 // AUTHOR:	Samuel Piecz
 // LOGON ID:	Z1732715
-// DUE DATE:	9/26/17
+// DUE DATE:	9/27/17
 //
 // PURPOSE:	Declaration for the Provider class, which represents
 //		information about a health care provider.
 //********************************************************************
 
+//Declaring the provider class
 class Provider
 {
+    // Private is not necessary but it looks nice
+    // Everything is private by default.
     private:
         //Data members
         char number[7] = {};
@@ -25,6 +28,7 @@ class Provider
         char zip_code[6] = {};
         char phone_number[15] = {};
 
+    // Declaring the public scope
     public:
         //Method prototypes
 
@@ -37,10 +41,10 @@ class Provider
         //Accessor method returns provider number data member
         const char* getProviderNumber() { return number; }
 
-        //Accessor method that returns specialty data member
+        //Accessor method that returns provider name data member
         const char*  getProviderName() { return name; }
 
-        //Accessor method that returns specialty
+        //Accessor method that returns provider specialty data member
         const char* getProviderSpecialty() { return specialty; }
 };
 
